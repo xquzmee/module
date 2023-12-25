34617be2dde1caf51718d05083b6be13850b3bb1
xquzmee <m.chernikov@istu.edu.ua>
2023-12-25 10:24:13 +0200

    singletonpatterns.cpp

---
+++ singletonpatterns.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <string>
+
+class University {
+private:
+    // Приватний конструктор, щоб заборонити створення об'єктів ззовні класу
+    University() {
+        // Ініціалізація даних за необхідності
+    }
+
+    // Приватний статичний екземпляр класу
+    static University* instance;
+
+public:
+    // Метод для отримання єдиного екземпляра класу
+    static University* getInstance() {
+        if (!instance) {
+            instance = new University();
+        }
+        return instance;
+    }
+
+    // Інші методи та дані класу
+    void displayInfo() {
+        std::cout << "Welcome to the University!" << std::endl;
+    }
+
+    // Додаткові методи та дані класу
+};
+
+// Ініціалізація статичної змінної
+University* University::instance = nullptr;
+
+int main() {
+    // Використання єдиного екземпляра класу
+    University* myUniversity = University::getInstance();
+    myUniversity->displayInfo();
+
+    // Ще один спроба отримати екземпляр класу
+    University* anotherUniversity = University::getInstance();
+    anotherUniversity->displayInfo();
+
+    // Порівняння адрес об'єктів, повинно бути однаковою
+    std::cout << "Address of myUniversity: " << myUniversity << std::endl;
+    std::cout << "Address of anotherUniversity: " << anotherUniversity << std::endl;
+
+    return 0;
+}
